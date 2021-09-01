c663587369a7fb250ea29190a1ed2ae49fbe4b49
Nicholas Dimmick <88209063+SaintNickyD@users.noreply.github.com>
2021-09-01 12:14:39 -0400

    Add files via upload

---
+++ src/YourAccount.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./YourAccount.css";
+
+
+function YourAccount() {
+    return (
+        <div className="yourAccount__Options">
+            <p>
+                Welcome To User's Account
+            </p>
+            
+        </div>
+    )
+}
+
+export default YourAccount
+
